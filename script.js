be3c3714ea59ae3b7c4ea8a405a152874b41626f
Oscar Garcia <oscare_gs@hotmail.com>
2022-08-05 13:53:06 -0500

    Main functionality ok

--- script.js
+++ script.js
@@ -1,21 +1,71 @@
 const grid = document.querySelector("#grid");
 
-let gridSize = 16;
+
+
+let gridSize = 50;
+// let s = 500 / (gridSize);
+
+// grid.style.height = (s * 16) + "px";
+// grid.style.width = (s * 16) + "px";
 
 let squaresArray = [];
 
-for (let i = 0; i < gridSize * gridSize; i++){
-    squaresArray.push(document.createElement("div"));
+for (let i = 0; i < gridSize; i++){
+    grid.appendChild(document.createElement("div"));
+
 }
 
-squaresArray.forEach((square)  => {
+let rows = grid.childNodes;
+let rowsA = Array.from(rows);
+rowsA.shift();
 
-    square.classList.add("square");
-    square.addEventListener('mouseover', function(e) {
-        e.target.style.background = "black";
-    });
-    grid.appendChild(square);
+console.log(rows);
+console.log(rowsA);
+
+rowsA.forEach((row) => {
+
+    row.classList.add("row")
+    row.style.height = "30px";
+
+    for (let j = 0; j < gridSize; j++){
+        row.appendChild(document.createElement("div"))
+    }
+
+    let sqrs = row.childNodes;
+    let sqrA = Array.from(sqrs);
     
 
+    console.log(sqrA);
+
+    sqrA.forEach((sqr) => {
+        sqr.classList.add("square");
+        sqr.addEventListener('mouseover', function(e) {
+            e.target.style.background = "black";
+        });
+    });
+
+    
+    
 });
+
+
+
+
+
+
+// squaresArray.forEach((row)  => {
+
+//     row.classList.add("flexContainerHorizontal");
+//     grid.appendChild(row);
+
+//    for (int j = 0; j < gridSize; j++){
+       
+//    }
+
+   
+
+
+    
+  
+
 
