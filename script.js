8c061c01b5c2777be0bd5906dd9647cc4c1dc584
Oscar Garcia <oscare_gs@hotmail.com>
2022-08-08 09:48:36 -0500

    Button for grid size clears previous grid and creater new

--- script.js
+++ script.js
@@ -2,66 +2,71 @@ const grid = document.querySelector("#grid");
 
 const gridSizeButton = document.querySelector("#gridSizeButton");
 
-let gridSize = 50;
+
 gridSizeButton.addEventListener("click", () =>{
-    gridSize = prompt("Enter size of grid", "50");
-});
+    let gridSize = prompt("Enter size of grid", "50");
 
-
-
-let squaresArray = [];
-
-for (let i = 0; i < gridSize; i++){
-    grid.appendChild(document.createElement("div"));
-
-}
-
-let rows = grid.childNodes;
-let rowsA = Array.from(rows);
-rowsA.shift();
-
-console.log(rows);
-console.log(rowsA);
-
-rowsA.forEach((row) => {
-
-    row.classList.add("row")
-    row.style.height = "30px";
-
-    for (let j = 0; j < gridSize; j++){
-        row.appendChild(document.createElement("div"))
+    if (gridSize > 100){
+        alert("You cant enter a grid size greater than 100");
+        return;
     }
 
-    let sqrs = row.childNodes;
-    let sqrA = Array.from(sqrs);
-    
+    clearGrid();
 
-    console.log(sqrA);
-
-    sqrA.forEach((sqr) => {
-        sqr.classList.add("square");
-        sqr.addEventListener('mouseover', function(e) {
-            e.target.style.background = "black";
-        });
-    });
-
-    
-    
+    createGrid(gridSize);
 });
 
 
 
+function clearGrid(){
+    while(grid.firstChild) {
+        grid.removeChild(grid.firstChild);
+      }
+}
+
+
+function createGrid(gridSize){
+    for (let i = 0; i < gridSize; i++){
+        grid.appendChild(document.createElement("div"));
+    
+    }
+    
+    let rows = grid.childNodes;
+    let rowsA = Array.from(rows);
+    rowsA.shift();
+    
+    console.log(rows);
+    console.log(rowsA);
+    
+    rowsA.forEach((row) => {
+    
+        row.classList.add("row")
+        row.style.height = "30px";
+    
+        for (let j = 0; j < gridSize; j++){
+            row.appendChild(document.createElement("div"))
+        }
+    
+        let sqrs = row.childNodes;
+        let sqrA = Array.from(sqrs);
+        
+    
+        console.log(sqrA);
+    
+        sqrA.forEach((sqr) => {
+            sqr.classList.add("square");
+            sqr.addEventListener('mouseover', function(e) {
+                e.target.style.background = "black";
+            });
+        });
+    
+        
+        
+    });
+}
 
 
 
-// squaresArray.forEach((row)  => {
-
-//     row.classList.add("flexContainerHorizontal");
-//     grid.appendChild(row);
-
-//    for (int j = 0; j < gridSize; j++){
-       
-//    }
 
    
 
